3088c506cf01634fba97cc9d2ac996998072c7c6
dine-7 <86520495+dine-7@users.noreply.github.com>
2024-04-04 11:53:18 +0530

    Update error_handlers.py

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -32,8 +32,7 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
-        ),
+        status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message ),
         status.HTTP_400_BAD_REQUEST,
     )
 
@@ -45,10 +44,7 @@ def not_found(error):
     app.logger.warning(message)
     return (
         jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
-        status.HTTP_404_NOT_FOUND,
-    )
-
-
+        status.HTTP_404_NOT_FOUND)
 @app.errorhandler(status.HTTP_405_METHOD_NOT_ALLOWED)
 def method_not_supported(error):
     """Handles unsupported HTTP methods with 405_METHOD_NOT_SUPPORTED"""
